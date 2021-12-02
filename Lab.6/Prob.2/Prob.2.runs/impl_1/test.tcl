b5df351dcd2e508b4085ad3d1c03509c913ac572
Sh4wn-Fr4nk1in <74861140+Sh4wn-Fr4nk1in@users.noreply.github.com>
2021-12-02 17:15:27 +0800

    Add Lab 6 report and submit

--- Lab.6/Prob.2/Prob.2.runs/impl_1/test.tcl
+++ Lab.6/Prob.2/Prob.2.runs/impl_1/test.tcl
@@ -61,99 +61,13 @@ proc step_failed { step } {
 }
 
 
-start_step init_design
-set ACTIVE_STEP init_design
-set rc [catch {
-  create_msg_db init_design.pb
-  set_param chipscope.maxJobs 2
-  create_project -in_memory -part xc7a100tcsg324-1
-  set_property design_mode GateLvl [current_fileset]
-  set_param project.singleFileAddWarning.threshold 0
-  set_property webtalk.parent_dir D:/Code/Digital-Logic-Lab/Lab.6/Prob.2/Prob.2.cache/wt [current_project]
-  set_property parent.project_path D:/Code/Digital-Logic-Lab/Lab.6/Prob.2/Prob.2.xpr [current_project]
-  set_property ip_output_repo D:/Code/Digital-Logic-Lab/Lab.6/Prob.2/Prob.2.cache/ip [current_project]
-  set_property ip_cache_permissions {read write} [current_project]
-  add_files -quiet D:/Code/Digital-Logic-Lab/Lab.6/Prob.2/Prob.2.runs/synth_1/test.dcp
-  read_xdc D:/Code/Digital-Logic-Lab/Lab.6/Prob.2/Prob.2.srcs/constrs_1/new/test.xdc
-  link_design -top test -part xc7a100tcsg324-1
-  close_msg_db -file init_design.pb
-} RESULT]
-if {$rc} {
-  step_failed init_design
-  return -code error $RESULT
-} else {
-  end_step init_design
-  unset ACTIVE_STEP 
-}
-
-start_step opt_design
-set ACTIVE_STEP opt_design
-set rc [catch {
-  create_msg_db opt_design.pb
-  opt_design 
-  write_checkpoint -force test_opt.dcp
-  create_report "impl_1_opt_report_drc_0" "report_drc -file test_drc_opted.rpt -pb test_drc_opted.pb -rpx test_drc_opted.rpx"
-  close_msg_db -file opt_design.pb
-} RESULT]
-if {$rc} {
-  step_failed opt_design
-  return -code error $RESULT
-} else {
-  end_step opt_design
-  unset ACTIVE_STEP 
-}
-
-start_step place_design
-set ACTIVE_STEP place_design
-set rc [catch {
-  create_msg_db place_design.pb
-  if { [llength [get_debug_cores -quiet] ] > 0 }  { 
-    implement_debug_core 
-  } 
-  place_design 
-  write_checkpoint -force test_placed.dcp
-  create_report "impl_1_place_report_io_0" "report_io -file test_io_placed.rpt"
-  create_report "impl_1_place_report_utilization_0" "report_utilization -file test_utilization_placed.rpt -pb test_utilization_placed.pb"
-  create_report "impl_1_place_report_control_sets_0" "report_control_sets -verbose -file test_control_sets_placed.rpt"
-  close_msg_db -file place_design.pb
-} RESULT]
-if {$rc} {
-  step_failed place_design
-  return -code error $RESULT
-} else {
-  end_step place_design
-  unset ACTIVE_STEP 
-}
-
-start_step route_design
-set ACTIVE_STEP route_design
-set rc [catch {
-  create_msg_db route_design.pb
-  route_design 
-  write_checkpoint -force test_routed.dcp
-  create_report "impl_1_route_report_drc_0" "report_drc -file test_drc_routed.rpt -pb test_drc_routed.pb -rpx test_drc_routed.rpx"
-  create_report "impl_1_route_report_methodology_0" "report_methodology -file test_methodology_drc_routed.rpt -pb test_methodology_drc_routed.pb -rpx test_methodology_drc_routed.rpx"
-  create_report "impl_1_route_report_power_0" "report_power -file test_power_routed.rpt -pb test_power_summary_routed.pb -rpx test_power_routed.rpx"
-  create_report "impl_1_route_report_route_status_0" "report_route_status -file test_route_status.rpt -pb test_route_status.pb"
-  create_report "impl_1_route_report_timing_summary_0" "report_timing_summary -max_paths 10 -file test_timing_summary_routed.rpt -pb test_timing_summary_routed.pb -rpx test_timing_summary_routed.rpx -warn_on_violation "
-  create_report "impl_1_route_report_incremental_reuse_0" "report_incremental_reuse -file test_incremental_reuse_routed.rpt"
-  create_report "impl_1_route_report_clock_utilization_0" "report_clock_utilization -file test_clock_utilization_routed.rpt"
-  create_report "impl_1_route_report_bus_skew_0" "report_bus_skew -warn_on_violation -file test_bus_skew_routed.rpt -pb test_bus_skew_routed.pb -rpx test_bus_skew_routed.rpx"
-  close_msg_db -file route_design.pb
-} RESULT]
-if {$rc} {
-  write_checkpoint -force test_routed_error.dcp
-  step_failed route_design
-  return -code error $RESULT
-} else {
-  end_step route_design
-  unset ACTIVE_STEP 
-}
-
 start_step write_bitstream
 set ACTIVE_STEP write_bitstream
 set rc [catch {
   create_msg_db write_bitstream.pb
+  set_param chipscope.maxJobs 2
+  open_checkpoint test_routed.dcp
+  set_property webtalk.parent_dir D:/Code/Digital-Logic-Lab/Lab.6/Prob.2/Prob.2.cache/wt [current_project]
   catch { write_mem_info -force test.mmi }
   write_bitstream -force test.bit 
   catch {write_debug_probes -quiet -force test}
