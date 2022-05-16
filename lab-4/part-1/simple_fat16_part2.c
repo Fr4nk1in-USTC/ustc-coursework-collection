11ff35b14d4eb50da4126b39c1522496ba95d38d
Fr4nk1in <fushen@mail.ustc.edu.cn>
2022-05-16 16:40:40 +0800

    Lab 4: Reformat part 1 initial code

--- lab-4/part-1/simple_fat16_part2.c
+++ lab-4/part-1/simple_fat16_part2.c
@@ -8,28 +8,30 @@
 
 // ------------------TASK3: 创建/删除文件夹-----------------------------------
 /**
- * @brief 创建path对应的文件夹
- * 
+ * @brief 创建 path 对应的文件夹
+ *
  * @param path 创建的文件夹路径
- * @param mode 文件模式，本次实验可忽略，默认都为普通文件夹
- * @return int 成功:0， 失败: POSIX错误代码的负值
+ * @param mode 文件模式, 本次实验可忽略, 默认都为普通文件夹
+ * @return int 成功:0,  失败: POSIX 错误代码的负值
  */
-int fat16_mkdir(const char *path, mode_t mode) {
-  /*** BEGIN ***/
-  /*** END ***/
-  return 0;
+int fat16_mkdir(const char *path, mode_t mode)
+{
+    /*** BEGIN ***/
+    /*** END ***/
+    return 0;
 }
 
 /**
- * @brief 删除path对应的文件夹
- * 
+ * @brief 删除 path 对应的文件夹
+ *
  * @param path 要删除的文件夹路径
- * @return int 成功:0， 失败: POSIX错误代码的负值
+ * @return int 成功: 0,  失败: POSIX 错误代码的负值
  */
-int fat16_rmdir(const char *path) {
-  /*** BEGIN ***/
-  /*** END ***/
-  return 0;
+int fat16_rmdir(const char *path)
+{
+    /*** BEGIN ***/
+    /*** END ***/
+    return 0;
 }
 
 // ------------------TASK4: 写文件-----------------------------------
@@ -45,9 +47,9 @@ int fat16_rmdir(const char *path) {
 int fat16_write(const char *path, const char *data, size_t size, off_t offset,
                 struct fuse_file_info *fi)
 {
-  /*** BEGIN ***/
-  /*** END ***/
-  return 0;
+    /*** BEGIN ***/
+    /*** END ***/
+    return 0;
 }
 
 //[TATODO][TASK4]
@@ -56,7 +58,7 @@ int fat16_write(const char *path, const char *data, size_t size, off_t offset,
  */
 int fat16_truncate(const char *path, off_t size)
 {
-  /*** BEGIN ***/
-  /*** END ***/
-  return 0;
+    /*** BEGIN ***/
+    /*** END ***/
+    return 0;
 }
