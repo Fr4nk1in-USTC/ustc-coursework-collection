388e0213c389e4e22621f69d9acd69516393909a
Fr4nk1in <fushen@mail.ustc.edu.cn>
2023-06-01 05:13:15 +0800

    [Lab 1] A* code clean

--- lab/lab-1/astar/src/astar.cpp
+++ lab/lab-1/astar/src/astar.cpp
@@ -10,10 +10,7 @@
 #include <unordered_set>
 #include <vector>
 
-auto now = std::chrono::high_resolution_clock::now;
-using ns = std::chrono::nanoseconds;
-
-using std::bitset;
+using std::cout;
 using std::endl;
 using std::greater;
 using std::ifstream;
@@ -29,9 +26,11 @@ using std::unordered_set;
 using std::vector;
 
 const size_t MAX_SIZE = 12;
+const size_t CASE_NUM = 10;
 
-using grid_t = bitset<MAX_SIZE * MAX_SIZE>;
-using ushort = unsigned short;
+auto now     = std::chrono::high_resolution_clock::now;
+using ns     = std::chrono::nanoseconds;
+using grid_t = std::bitset<MAX_SIZE * MAX_SIZE>;
 
 const char *INPUTS[] = {"../input/input0.txt", "../input/input1.txt",
                         "../input/input2.txt", "../input/input3.txt",
@@ -56,13 +55,13 @@ const char *OUTPUTS[] = {"../output/output0.txt", "../output/output1.txt",
 class action
 {
   public:
-    ushort x;
-    ushort y;
-    ushort shape;
+    unsigned x;
+    unsigned y;
+    unsigned shape;
 
     action(): x(0), y(0), shape(0) {}
 
-    action(ushort x_, ushort y_, ushort s): x(x_), y(y_), shape(s) {}
+    action(unsigned x_, ushort y_, ushort s): x(x_), y(y_), shape(s) {}
 
     friend ostream &operator<<(ostream &os, const action &a)
     {
@@ -71,14 +70,21 @@ class action
     }
 };
 
-ushort heuristic(const grid_t &g, ushort size)
+unsigned heuristic(const grid_t &g)
+{
+    int count = g.count();
+    return count / 3 + count % 3;
+}
+
+/* The heuristic below should be consistent, but I have no time to prove it */
+unsigned better_heuristic(const grid_t &g, ushort size)
 {
     vector<vector<unsigned>> dist(size, vector<unsigned>(size, 0));
     // Iterate over the grid to find all 1s with other 1s surrounded and mark it
-    for (ushort i = 0; i < size - 1; ++i) {
-        for (ushort j = 0; j < size - 1; ++j) {
-            ushort count = g[i * size + j] + g[i * size + j + 1]
-                         + g[(i + 1) * size + j] + g[(i + 1) * size + j + 1];
+    for (unsigned i = 0; i < size - 1; ++i) {
+        for (unsigned j = 0; j < size - 1; ++j) {
+            unsigned count = g[i * size + j] + g[i * size + j + 1]
+                           + g[(i + 1) * size + j] + g[(i + 1) * size + j + 1];
             if (count >= 3) {
                 dist[i][j]         |= 3;
                 dist[i + 1][j]     |= 3;
@@ -94,8 +100,8 @@ ushort heuristic(const grid_t &g, ushort size)
     }
     // Count the number of 1s, 2s and 3s
     float counts[3] = {0, 0, 0};
-    for (ushort i = 0; i < size; ++i) {
-        for (ushort j = 0; j < size; ++j) {
+    for (unsigned i = 0; i < size; ++i) {
+        for (unsigned j = 0; j < size; ++j) {
             if (g[i * size + j]) {
                 if (dist[i][j]) {
                     ++counts[dist[i][j] - 1];
@@ -106,8 +112,8 @@ ushort heuristic(const grid_t &g, ushort size)
         }
     }
 
-    ushort value = ceil(counts[0] + 7 * counts[1] / 15 + counts[2] / 3);
-    ushort sum   = g.count();
+    unsigned value = ceil(counts[0] + 7 * counts[1] / 15 + counts[2] / 3);
+    unsigned sum   = g.count();
 
     if ((value & 1) == (sum & 1)) {
         return value;
@@ -121,13 +127,13 @@ class node
   public:
     shared_ptr<node> parent;       // parent node
     action           from_parent;  // action from parent
-    ushort           g;            // steps from the root node
-    ushort           h;            // heuristic value
-    ushort           f;            // h + g
+    unsigned         g;            // steps from the root node
+    unsigned         h;            // heuristic value
+    unsigned         f;            // h + g
     grid_t           grid;         // grid of the current node
 
     // constructor for root node
-    node(const grid_t &g_, const ushort h_):
+    node(const grid_t &g_, const unsigned h_):
         parent(nullptr),
         grid(g_),
         g(0),
@@ -137,7 +143,7 @@ class node
 
     // constructor for non-root node
     node(const shared_ptr<node> &p, const action &a, const grid_t &g_,
-         const ushort h_):
+         const unsigned h_):
         parent(p),
         from_parent(a),
         grid(g_),
@@ -165,8 +171,8 @@ class solver
             throw invalid_argument("The input grid is too small");
         }
 
-        for (ushort i = 0; i < size * size; ++i) {
-            ushort temp;
+        for (unsigned i = 0; i < size * size; ++i) {
+            unsigned temp;
             file >> temp;
             grid[i] = temp;
         }
@@ -176,11 +182,11 @@ class solver
 
     void solve()
     {
-        auto root = make_shared<node>(grid, heuristic(grid, size));
+        auto root = make_shared<node>(grid, heuristic(grid));
 
-        priority_queue<shared_ptr<node>, vector<shared_ptr<node>>,
-                       greater<shared_ptr<node>>>
-            frontier;
+        using node_p = shared_ptr<node>;
+
+        priority_queue<node_p, vector<node_p>, greater<node_p>> frontier;
         frontier.push(root);
 
         unordered_set<grid_t> explored;
@@ -197,8 +203,7 @@ class solver
             explored.insert(current->grid);
 
             for (const auto &[g, a] : get_childrens(current->grid)) {
-                auto child =
-                    make_shared<node>(current, a, g, heuristic(g, size));
+                auto child = make_shared<node>(current, a, g, heuristic(g));
 
                 if (explored.find(g) == explored.end()) {  // not explored
                     frontier.push(child);
@@ -210,14 +215,14 @@ class solver
         return;
     }
 
-    bool validate()
+    bool verify()
     {
         grid_t g           = grid;
-        auto   take_action = [&g](const action &a, ushort size) {
-            ushort x   = a.x * size;
-            ushort y   = a.y;
-            ushort x_1 = x + ((a.shape < 3) ? -size : size);
-            ushort y_1 = y + ((a.shape == 2 or a.shape == 3) ? -1 : 1);
+        auto   take_action = [&g](const action &a, unsigned size) {
+            unsigned x   = a.x * size;
+            unsigned y   = a.y;
+            unsigned x_1 = x + ((a.shape < 3) ? -size : size);
+            unsigned y_1 = y + ((a.shape == 2 or a.shape == 3) ? -1 : 1);
 
             g[x + y]   = !g[x + y];
             g[x_1 + y] = !g[x_1 + y];
@@ -243,21 +248,21 @@ class solver
 
   private:
     grid_t         grid;
-    ushort         size;
+    unsigned       size;
     vector<action> path;  // optimal path
 
     unordered_map<grid_t, action> get_childrens(grid_t &g)
     {
-        ushort first_one_index;
-        for (ushort i = 0; i < size * size; ++i) {
+        unsigned first_one_index;
+        for (unsigned i = 0; i < size * size; ++i) {
             if (g[i]) {
                 first_one_index = i;
                 break;
             }
         }
 
-        ushort x = first_one_index / size;
-        ushort y = first_one_index % size;
+        unsigned x = first_one_index / size;
+        unsigned y = first_one_index % size;
 
         // 12 possible actions on the 1
         // +-------+-------+-------+-------+-------+-------+
@@ -269,44 +274,24 @@ class solver
         // | 0 # 0 | 0 # # | 0 # # | # # 0 | # # 0 | 0 # 0 |
         // | 0 # # | 0 0 # | 0 # 0 | 0 # 0 | # 0 0 | # # 0 |
         // +-------+-------+-------+-------+-------+-------+
-
+        // If the cell is not on the last row of grid, only the last 6 actions
+        // are effective. If the cell is on the last row, then only the first
+        // 6 actions are valid. Thus, only up to 6 actions will be generated.
         unordered_map<grid_t, action> actions_to_grids;
 
-        ushort valid_mask = 0x00f;  // One bit represents three actions
+        static int8_t   pos_x_offset[] = {-1, 0, 0, 0, -1, 0, 1, 0, 0, 0, 0, 1};
+        static int8_t   pos_y_offset[] = {0, -1, 0, 0, 0, 1, 0, 1, 0, 0, -1, 0};
+        static int8_t   arm_x_offset[] = {1,  -1, -1, -1, 1, -1,
+                                          -1, 1,  1,  1,  1, -1};
+        static int8_t   arm_y_offset[] = {-1, 1,  -1, 1,  1, -1,
+                                          1,  -1, 1,  -1, 1, -1};
+        static unsigned shapes[]       = {3, 1, 2, 1, 4, 2, 1, 3, 4, 3, 4, 2};
 
-        // The invalid actions mask for special (x, y)
-        ushort x_0_mask = 0x0003;  // 0011, [0 - 5]
-        ushort x_n_mask = 0x000c;  // 1100, [6 - 11]
-        ushort y_0_mask = 0x0009;  // 1001, [0 - 2, 9 - 11]
-        ushort y_n_mask = 0x0006;  // 0110, [3 - 8]
-
-        ushort invalid_mask = 0x0000;
-        if (x == 0) {
-            invalid_mask |= x_0_mask;
-        } else if (x == size - 1) {
-            invalid_mask |= x_n_mask;
-        }
-        if (y == 0) {
-            invalid_mask |= y_0_mask;
-        } else if (y == size - 1) {
-            invalid_mask |= y_n_mask;
-        }
-
-        valid_mask &= ~invalid_mask;
-
-        static int8_t pos_x_offset[] = {-1, 0, 0, 0, -1, 0, 1, 0, 0, 0, 0, 1};
-        static int8_t pos_y_offset[] = {0, -1, 0, 0, 0, 1, 0, 1, 0, 0, -1, 0};
-        static int8_t arm_x_offset[] = {1,  -1, -1, -1, 1, -1,
-                                        -1, 1,  1,  1,  1, -1};
-        static int8_t arm_y_offset[] = {-1, 1,  -1, 1,  1, -1,
-                                        1,  -1, 1,  -1, 1, -1};
-        static ushort shapes[]       = {3, 1, 2, 1, 4, 2, 1, 3, 4, 3, 4, 2};
-
-        auto gen = [&actions_to_grids, &g, x, y, this](ushort i) {
-            ushort pos_x = x + pos_x_offset[i];
-            ushort pos_y = y + pos_y_offset[i];
-            ushort arm_x = pos_x + arm_x_offset[i];
-            ushort arm_y = pos_y + arm_y_offset[i];
+        auto gen = [&actions_to_grids, &g, x, y, this](unsigned i) {
+            unsigned pos_x = x + pos_x_offset[i];
+            unsigned pos_y = y + pos_y_offset[i];
+            unsigned arm_x = pos_x + arm_x_offset[i];
+            unsigned arm_y = pos_y + arm_y_offset[i];
 
             grid_t new_g = g;
             action a(pos_x, pos_y, shapes[i]);
@@ -322,28 +307,28 @@ class solver
             actions_to_grids[new_g] = a;
         };
 
-        if (valid_mask & 0x0001) {
-            gen(0);
-            gen(1);
-            gen(2);
-        }
-
-        if (valid_mask & 0x0002) {
-            gen(3);
-            gen(4);
-            gen(5);
-        }
-
-        if (valid_mask & 0x0004) {
-            gen(6);
-            gen(7);
-            gen(8);
-        }
-
-        if (valid_mask & 0x0008) {
-            gen(9);
-            gen(10);
-            gen(11);
+        if (x != size - 1) {
+            if (y != 0) {
+                gen(9);
+                gen(10);
+                gen(11);
+            }
+            if (y != size - 1) {
+                gen(6);
+                gen(7);
+                gen(8);
+            }
+        } else {
+            if (y != 0) {
+                gen(0);
+                gen(1);
+                gen(2);
+            }
+            if (y != size - 1) {
+                gen(3);
+                gen(4);
+                gen(5);
+            }
         }
 
         return actions_to_grids;
@@ -352,7 +337,7 @@ class solver
     void set_solution(const shared_ptr<node> &goal)
     {
         path.resize(goal->g);
-        ushort           index   = 0;
+        unsigned         index   = 0;
         shared_ptr<node> current = goal;
         // reconstruct the path, order does not matter
         while (current->parent != nullptr) {
@@ -362,21 +347,21 @@ class solver
     }
 };
 
-int main(int argc, char *argv[])
+int main()
 {
-    for (int i = 0; i < 10; i++) {
+    for (int i = 0; i < CASE_NUM; i++) {
         ifstream input(INPUTS[i]);
 
         solver s = solver().from_file(input);
 
         auto start = now();
         s.solve();
-        auto end      = now();
-        ns   duration = end - start;
+        auto end = now();
 
-        std::cout << "Time taken for " << INPUTS[i] << ": "
-                  << duration.count() / 1e6 << " ms, "
-                  << "solution valid: " << s.validate() << endl;
+        ns duration = end - start;
+        cout << "Time taken for " << INPUTS[i] << ": " << duration.count() / 1e6
+             << " ms, "
+             << "solution verified: " << s.verify() << endl;
 
         ofstream output(OUTPUTS[i]);
         s.path_to(output);
